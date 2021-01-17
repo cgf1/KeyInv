4aa3df28b7b1e888b5e9b7e96346d3078764a192
Chris Faylor <cgf@realedsolutions.com>
2021-01-17 16:20:48 -0500

    * Bindings.xml: Add key bindings 5 - 10. * KeyInv.lua: Ditto.  Allow use of collectibles, as shown via inventory.  Use LibChat. (Key): Detect collectible. (assign): Ditto. (addmenu): Change parameter to 'id' since it isn't necessarily a string.  Accommodate colletibles. (rightclick): Notice when we've right clicked on a collectible. * KeyInv.txt: Update API version.  Add depend on LibChat.

--- KeyInv.lua
+++ KeyInv.lua
@@ -15,7 +15,11 @@ KeyInv = setmetatable(x, x)
 KeyInv.KeyInv = KeyInv
 setfenv(1, KeyInv)
 
+local LCM = LibChatMessage
+local chat = LCM.Create('KeyInv', 'KeyInv')
+
 local name = 'KeyInv'
+local version = "1.0"
 local settings_version = 1
 
 local saved = {
@@ -27,48 +31,67 @@ local function emptyfunc()
 end
 
 local dbg = emptyfunc
-local dformat = emptyfunc
 
 local function inventory(name)
 	-- get the character bag size
 	local n = GetBagSize(BAG_BACKPACK)
 	
-	-- iterate through backpack bag to find all matching items, and add their counts to the total
+	-- iterate through backpack bag to find first matching items
 	for i = 0, n do
-		local iname = GetItemName(BAG_BACKPACK, i)
-                if name == iname then
-                    local success = CallSecureProtected("UseItem", BAG_BACKPACK, i)
-                    df("%s success: %s", iname, tostring(success))
-                    return i
-                end
+            local iname = GetItemName(BAG_BACKPACK, i)
+            if name == iname then
+                local success = CallSecureProtected("UseItem", BAG_BACKPACK, i)
+                chat:Print("%s success: %s", iname, tostring(success))
+                return i
+            end
 	end
         df("|c00ffffInventory item \"%s\" not found", name)
 end
 
 function Key(n)
-    if saved.key[n] then
-        inventory(saved.key[n])
+    local todo = saved.key[n]
+    if not todo then
+        -- oh well
+    elseif type(todo) == 'number' then
+        UseCollectible(todo)
+    else
+        inventory(todo)
     end
 end
 
-local function assign(n, iname)
-    saved.key[n] = iname
-    df('|c00ff11Assigned key %d to "%s"', n, iname)
+local function assign(n, id)
+    saved.key[n] = id
+    local iname
+    if type(id) == 'number' then
+        iname = GetCollectibleName(id)
+    else
+        iname = id
+    end
+    chat:Printf('|c00ff11Assigned key %d to "%s"', n, tostring(iname))
 end
 
-local function addmenu(iname)
+local function addmenu(id)
     local entries = {}
-    for i = 1, 5 do
-        entries[i] = {label = string.format('Key %d', i), callback = function() assign(i, iname) end}
+    for i = 1, 10 do
+        entries[i] = {label = string.format('Key %d', i), callback = function() assign(i, id) end}
     end
     AddCustomSubMenuItem("Assign to shortcut key", entries)
     ShowMenu(control)
 end
 
 local function rightclick(control)
-    local _, n = ZO_Inventory_GetBagAndIndex(control)
-    local iname = GetItemName(BAG_BACKPACK, n)
-    zo_callLater(function () addmenu(iname) end, 0)
+    local iscollection = ZO_InventorySlot_GetType(control) == SLOT_TYPE_COLLECTIONS_INVENTORY 
+    local id
+    if iscollection then
+        id = control.collectibleId
+    else
+        local bag, ix = ZO_Inventory_GetBagAndIndex(control)
+        if not bag then
+            return
+        end
+        id = GetItemName(bag, ix)
+    end
+    zo_callLater(function () addmenu(id) end, 0)
 end
 
 local function onloaded(_, addon_name)
@@ -81,7 +104,6 @@ local function onloaded(_, addon_name)
     KeyInv_Saved = saved
     if saved.debug then
         dbg = df
-        dformat = string.format
     else
         dbg = emptyfunc
         dbg = emptyfunc
@@ -93,17 +115,15 @@ local function onloaded(_, addon_name)
 	    -- nothing to do
 	elseif n == 'true' or n == 'on' then
 	    dbg = df
-            dformat = string.format
             saved.debug = true
 	else
 	    dbg = emptyfunc
-            dformat = emptyfunc
             saved.debug = false
 	end
 	d("KeyInv debugging: " .. tostring(dbg == df))
     end
     SLASH_COMMANDS['/kidump'] = function(n)
-        for i = 1, 5 do
+        for i = 1, 10 do
             if saved.key[i] then
                 df("key %d: %s", i, saved.key[i])
             end
@@ -116,6 +136,11 @@ local function onloaded(_, addon_name)
     ZO_CreateStringId('SI_BINDING_NAME_KEYINV_HOTKEY3', 'Inventory shortcut key 3')
     ZO_CreateStringId('SI_BINDING_NAME_KEYINV_HOTKEY4', 'Inventory shortcut key 4')
     ZO_CreateStringId('SI_BINDING_NAME_KEYINV_HOTKEY5', 'Inventory shortcut key 5')
+    ZO_CreateStringId('SI_BINDING_NAME_KEYINV_HOTKEY6', 'Inventory shortcut key 6')
+    ZO_CreateStringId('SI_BINDING_NAME_KEYINV_HOTKEY7', 'Inventory shortcut key 7')
+    ZO_CreateStringId('SI_BINDING_NAME_KEYINV_HOTKEY8', 'Inventory shortcut key 8')
+    ZO_CreateStringId('SI_BINDING_NAME_KEYINV_HOTKEY9', 'Inventory shortcut key 9')
+    ZO_CreateStringId('SI_BINDING_NAME_KEYINV_HOTKEY10', 'Inventory shortcut key 10')
 end
 
 EVENT_MANAGER:RegisterForEvent(name, EVENT_ADD_ON_LOADED, onloaded)
